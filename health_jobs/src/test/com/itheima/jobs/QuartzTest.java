4a51b7e49c5cffe0a20bff4d508af23f5f6a86ed
hongbo <1278907448@qq.com>
2020-01-26 13:43:46 +0800

    修改了一下

--- health_jobs/src/test/com/itheima/jobs/QuartzTest.java
+++ health_jobs/src/test/com/itheima/jobs/QuartzTest.java
@@ -5,5 +5,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class QuartzTest {
     public static void main(String[] args) {
         new ClassPathXmlApplicationContext("application-jobs.xml");
+
     }
 }
